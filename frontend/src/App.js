e835ec944b86c32f53035794ed0f8f40a9e98856
Rafael Nascimento <rafael.doug@gmail.com>
2021-09-22 18:30:19 -0300

    Ajusta schemas, adição e deleção de tasks e adiciona Componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,9 @@ import {
   Link
 } from 'react-router-dom';
 
-import { useState, useEffect } from 'react';
-import TodoList from './components/Todo/TodoList';
-import CategoriesList from './components/Categories/CategoriesList';
 import Header from './components/Header';
+import CategoriesList from './components/Categories/CategoriesList';
+import TodoList from './components/Todo/TodoList';
 
 const HOME_TITLE = "To-Do List";
 
@@ -18,14 +17,13 @@ function App() {
     <Router>
       <div className="principal">
         <Link to="/">
-          <Header title={HOME_TITLE}/>
+          <Header title={HOME_TITLE} />
         </Link>
         <Switch>
           <Route path="/" exact={true}>
-            {/* <CategoriesList categories={categories} subtitle={HOME_SUBTITLE}/> */}
             <CategoriesList />
           </Route>
-          <Route path="/lists/:id/tasks">
+          <Route path="/tasks/lists/:id">
             <TodoList />
           </Route>
         </Switch>
--- frontend/src/components/Categories/CategoriesList.js
+++ frontend/src/components/Categories/CategoriesList.js
@@ -4,6 +4,7 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import axios from 'axios';
 import './styles.css';
+import Input from "../Input";
 
 const API_URL = "http://localhost:5000";
 
@@ -12,20 +13,21 @@ const HOME_SUBTITLE = "Suas Listas";
 export default function CategoriesList() {
 
   const [categories, setCategories] = useState([]);
-  const [newCategory, setNewCategory] = useState({});
+  const [newCategory, setNewCategory] = useState('');
   const [popupActive, setPopupActive] = useState(false);
 
   useEffect(() => {
     async function carregarCategorias() {
-      const {data} = await axios.get(`${API_URL}/lists`);
+      const { data } = await axios.get(`${API_URL}/lists`);
       setCategories(data);
     }
     carregarCategorias();
   }, []);
 
   const addCategory = async () => {
-    const addedCategory = await axios.post(`${API_URL}/lists/new`, {name: newCategory});
-    setCategories([...categories, addedCategory]);
+    const { data } = await axios.post(`${API_URL}/lists/new`, { name: newCategory });
+    setCategories([...categories, data]);
+    setNewCategory('');
     setPopupActive(false);
   }
 
@@ -35,20 +37,17 @@ export default function CategoriesList() {
     setCategories(newCategories);
   }
 
-  console.log(categories);
-
   return (
     <>
       <h4>{HOME_SUBTITLE}</h4>
       {categories.map(c => (
         <div className="category-container" key={c._id}>
-          <Link className="category" to={`/lists/${c._id}/tasks`} >
+          <Link className="category" to={`/tasks/lists/${c._id}`} >
             <div className="text">{c.name}</div>
           </Link>
-          <div>
+          <div className="btn-container">
             <IconButton aria-label="delete" className="delete-list" onClick={() => deleteCategory(c._id)} >
-            {/* <IconButton aria-label="delete" className="delete-list" > */}
-              <DeleteIcon fontSize="small" color="secondary" />
+              <DeleteIcon fontSize="medium" color="secondary" />
             </IconButton>
           </div>
         </div>
@@ -59,17 +58,15 @@ export default function CategoriesList() {
       {popupActive ? (
         <div className="popup">
           <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
-          <div className="content">
-            <h3>Adicionar Categoria</h3>
-            <input
-              type="text"
-              className="add-input"
-              onChange={e => setNewCategory(e.target.value)}
-              value={newCategory} />
-            <div className="button" onClick={addCategory}>Criar Categoria</div>
-          </div>
+          <Input
+            title={"Adicionar Lista"}
+            buttonLabel={"Criar Lista"}
+            setValue={e => setNewCategory(e.target.value)}
+            newItem={newCategory}
+            addItem={addCategory} />
         </div>
       ) : ''}
+
     </>
   )
 
